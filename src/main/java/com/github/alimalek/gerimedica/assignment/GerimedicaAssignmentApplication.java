212c91488d0dc92900b5084109d2104f76682210
Ali Malek <ali.malek.71@gmail.com>
2024-09-28 15:26:32 +0200

    chore: init project

---
+++ src/main/java/com/github/alimalek/gerimedica/assignment/GerimedicaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.github.alimalek.gerimedica.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerimedicaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerimedicaAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/alimalek/gerimedica/assignment/GerimedicaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alimalek.gerimedica.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerimedicaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
